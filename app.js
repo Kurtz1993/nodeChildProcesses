8754ded0a7256e473ae8ab8b43bcdee6eb98e25c
Luis Rogelio Hernández López <rogelio.hernandez@msw.com.mx>
2015-03-06 11:26:10 -0600

    Analyzing the object child_process.spawn

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-var cp = require('child_process'); // Utiliza child processes
-// Fork se utiliza cuando se ejecutará otro script como child process
-var n = cp.fork(__dirname + '/crypto.js');
-// exec se utiliza para ejecutar comandos del shell.
-var exec = cp.exec;
+// var cp = require('child_process'); // Utiliza child processes
+// // Fork se utiliza cuando se ejecutará otro script como child process
+// // exec se utiliza para ejecutar comandos del shell.
+// var exec = cp.exec;
 
-x = exec('echo "Hello, world"', function (error, stdout, stderr){
-	console.log("Stdout: ", stdout);	// Salida del comando
-	console.log("Stderr: ", stderr);	// Errores del comando
-	if(error !== null){					// Errores del comando exec
-		console.log("exec error: ", error)
-	}
-});
+// x = exec('echo "Hello, world"', function (error, stdout, stderr){
+// 	console.log("Stdout: ", stdout);	// Salida del comando
+// 	console.log("Stderr: ", stderr);	// Errores del comando
+// 	if(error !== null){					// Errores del comando exec
+// 		console.log("exec error: ", error)
+// 	}
+// });
 
-n.send({text: "Original message"});	// Envia un mensaje (objeto) al child process.
+// // var n = cp.fork(__dirname + '/crypto.js');
+// // n.send({text: "Original message"});	// Envia un mensaje (objeto) al child process.
 
-// Cuando recibe un mensaje del child process...
-n.on('message', function(m){
-	console.log("PARENT got message: ", m);	// Imprime el mensaje
-	n.kill();	// Termina el proceso.
-	console.log("Process terminated..."); 
-});
+// // Cuando recibe un mensaje del child process...
+// var n = cp.fork(__dirname + '/crypto.js');
+// for(i=0; i<5; i++){
+// 	n.send({text: "Original message"});	// Envia un mensaje (objeto) al child process.	
+// }
+// n.on('message', function(m){
+// 	console.log("PARENT got message: ", m);	// Imprime el mensaje
+// 	process.kill(n.pid);	// Termina el proceso.
+// 	console.log("Process terminated..."); 
+// });
+
+var spawn = require("child_process").spawn;
+var workers = [];
+
+var killWorkers = function() {
+  workers.forEach(function(worker) {
+    process.kill(worker);
+  });
+};
+
+process.on("uncaughtException", killWorkers);
+process.on("SIGINT", killWorkers);
+process.on("SIGTERM", killWorkers);
+// for(i=0; i<25; i++){
+var new_worker = spawn("tail", ["-f", "/dev/null"]);
+workers.push(new_worker);
+console.dir(new_worker)
+console.log(new_worker.pid)
+// }
